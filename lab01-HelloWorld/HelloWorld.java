8620b35fc80e012ac0443a86e23a753acd3d4534
cgschiff <cgs319@lehigh.edu>
2016-01-29 16:47:04 +0000

    updated HelloWorld

---
+++ lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,27 @@
+//
+//Connor Schiff
+//Hello World Java Program
+//
+// first compile the program
+//  javac.HelloWorld.java
+// run the program
+//  java HelloWorld//
+
+//  define a class
+public class HelloWorld{
+
+// add a main method
+    public static void main(String[ ] args){
+        
+//  print the statement with the box around it
+    System.out.println("#######################");
+    System.out.println("#                     #");
+    System.out.println("#  Hello World 2016!  #");
+    System.out.println("#                     #");
+    System.out.println("#######################");
+    System.out.println("test");
+    
+    //test/testtest
+    
+    }
+}
